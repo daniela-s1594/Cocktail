936ce3b1d1b20f1d643938f1289505021a924229
DanielaMV <danielamvirviescas2004@gmail.com>
2024-01-02 10:31:34 -0500

    union del header y las bebidas, tambien estilo para la imagen,se elimino de la vista el nombre y el ver detalle y se añadio un efecto para mostrar la informacion al darle hover en la imagen.DMV

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,13 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Drinks from "./components/Drink/Drinks";
-import Details from "./components/details/Details";
 import Header from "./layout/header/header";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Drinks />} />
-        <Route path="/drink/:IdDrink" element={<Details />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Drinks />
+    </>
   );
 }
 
